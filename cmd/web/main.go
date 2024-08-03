9fde0feeabe3e3f54682686c5754f9113eb75b20
anandg112 <anandg112@gmail.com>
2024-08-03 18:00:32 -0400

    feat: add configuration and html templates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,33 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+
+	var cfg config
+
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+
+	flag.Parse()
 	// Register the two new handler functions and corresponding URL patterns with
 	// the servemux, in exactly the same way that we did before.
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir(cfg.staticDir))
+
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
 
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("starting server on :4000")
+	log.Printf("starting server on %s", cfg.addr)
 
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(cfg.addr, mux)
 	log.Fatal(err)
 }
